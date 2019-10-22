1d94e5b0ebbad27f05ba440b7031e83d04434e6a
Pedro Huertas <pmh242@gmail.com>
2019-10-22 23:49:17 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,12 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array = []
+  value_to_find = 
 end
 
 def find_max_value(array)
-  # Add your solution here
+  array = []
 end
 
 def find_min_value(array)
-  # Add your solution here
+  array = []
 end
