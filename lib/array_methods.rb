f89b2630f7a003ae004715e872c02ba5d6fc324a
Pedro Huertas <pmh242@gmail.com>
2019-10-23 00:07:21 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,9 @@
 def find_element_index(array, value_to_find)
   array.length.times do |count|
-    if array[count] == value_to_find
-      return count
-    end
- end
+  if array[count] == value_to_find
+    return count
+  end
+end
  
 def find_max_value(array)
   array = []
